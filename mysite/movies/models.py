93a5e1917862e418cb84d1d98631e6a595fe40e5
aleksandragorczyca <olinkx1234567@gmail.com>
2022-06-09 20:50:02 +0200

    male_zmiany

--- mysite/movies/models.py
+++ mysite/movies/models.py
@@ -20,7 +20,7 @@ class Movie(models.Model):
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
     actor = models.ManyToManyField(Actor)
     movie_name = models.CharField(max_length=200)
-    description = models.TextField(max_length=400)
+    description = models.TextField()
 
     def __str__(self):
         return self.movie_name
--- mysite/movies/views.py
+++ mysite/movies/views.py
@@ -1,9 +1,4 @@
-from django.http import HttpResponse
-from re import template
-from multiprocessing import context
 from django.shortcuts import render
-from django.template import loader
-
 
 
 from .models import Genre, Movie, Actor
@@ -11,26 +6,22 @@ from .models import Genre, Movie, Actor
 
 def index(request):   
     index_list = 'Genre', 'Actor', 'Movie'
-    template = loader.get_template('movies/index.html')
     context = {'index_list': index_list}
-    return HttpResponse(template.render(context, request))
+    return render(request, 'movies/index.html', context)
     
 
 def genre(request):
     genre_list = Genre.objects.all
-    template = loader.get_template('movies/genre.html')
-    context = {'genre_list':genre_list}
-    return HttpResponse(template.render(context, request))
+    context = {'genre_list': genre_list}
+    return render(request, 'movies/genre.html', context)
 
 
 def movie(request):
     movie_list = Movie.objects.all
-    template = loader.get_template('movies/movie.html')
     context = {'movie_list':movie_list}
-    return HttpResponse(template.render(context, request))
+    return render(request, 'movies/movie.html', context)
 
 def actor(request):
     actor_list = Actor.objects.all
-    template = loader.get_template('movies/actor.html')
     context = {'actor_list':actor_list}
-    return HttpResponse(template.render(context, request))
+    return render(request, 'movies/actor.html',context)
